8f8fce2b7e91e20d6f23eeed7a5805ba6edd77cd
Vidhya Gholkar <me@server.fake>
2014-01-26 11:47:41 +0000

    typo, also return gcd

--- ehcf.js
+++ ehcf.js
@@ -4,7 +4,7 @@ function ehcf(a, b){
     if (b === 0) return [1, 0, a]
 
     t = ehcf(b, a % b)
-	return [t[1] , t[0] - t[1] * Math.floor(a/b)]
+	return [t[1] , t[0] - t[1] * Math.floor(a/b), t[2]]
 }
 
 
